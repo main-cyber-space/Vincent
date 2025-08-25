9f8ab1f42c07744c762924407985acd9c6d056ea
realpaygo <ebukamaxwell001@gmail.com>
2025-08-25 20:41:28 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -31,7 +31,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("2048684033")
+    navigator.clipboard.writeText("8941127171")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -91,7 +91,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">2048684033</p>
+                <p className="font-bold">8941127171</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -103,12 +103,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">KUDA MFB</p>
+              <p className="font-bold">Palmpay</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">Chukwudi Kenechukwu Pascal</p>
+              <p className="font-bold">RIDWAN OPEYEMI OLABAMIJI</p>
             </div>
           </div>
 
